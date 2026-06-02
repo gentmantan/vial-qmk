4e9c003c28701665c65c901eaf70c527a906a9b7
Gentman Tan <gentmantan@gmail.com>
2026-06-02 12:17:08 -0400

    Enable overlap mouse key control

--- keyboards/cheapino/config.h
+++ keyboards/cheapino/config.h
@@ -33,3 +33,5 @@
 #define RGBLIGHT_DEFAULT_HUE 128 // Sets the default hue value, if none has been set
 #define RGBLIGHT_DEFAULT_SAT 128 // Sets the default saturation value, if none has been set
 #define RGBLIGHT_DEFAULT_VAL 32 // Sets the default brightness value, if none has been set
+
+#define MOUSEKEY_OVERLAP_RESET
